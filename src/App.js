cf0a9890cfa709915fdaa060c8691dbf9cd88c31
gulraeezgulshan <gulraizgulshan@gmail.com>
2022-06-29 23:04:56 +0500

    new changes

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-         Welcome to SHU 2333
+         I am Gulraiz Gulshan
         </p>
         <a
           className="App-link"
